d64878b88946f9c761a7cf4bc19e357490bde2a9
Ian Campbell <icampbell47@gatech.edu>
2025-11-10 22:28:24 -0500

    Created core pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className = "home-page">
+            <header>Welcome to my personal website</header>
+            <main>
+                <p>This is the home page where you can find information about me and my projects.</p>
+            </main>
+            <footer>
+                <p>© 2025 My Personal Website</p>
+            </footer>
+        </div>
+    )
+}
+export default Home;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Projects = () => {
+    return(
+        <div className = "projects-page">
+            <header>Projects</header>
+            <main>
+                <p>This page contains a list of my projects.</p>
+            </main>
+            <footer>
+                <p>© 2025 My Personal Website</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Projects;
